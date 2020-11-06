ba736fc98c8bdf1d7d827b381aeb28b060fa1aeb
Александр <buga-91@inbox.ru>
2020-11-07 03:27:20 +0600

    initial commit

---
+++ easy-school/easy-school/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  easy-school
+//
+//  Created by Александр on 07.11.2020.
+//  Copyright © 2020 AlexBugatti. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ easy-school/easy-school/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easy-school
+//
+//  Created by Александр on 07.11.2020.
+//  Copyright © 2020 AlexBugatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
